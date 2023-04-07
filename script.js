9f5d99a6e35051a686d8c9b600086b4cce6ec578
Mikko <realmiks27@gmail.com>
2023-04-06 20:51:15 -0700

    Create remove button that removes books/'cards'

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const body = document.querySelector('body');
+let myLibrary = [];
 
+const body = document.querySelector('body');
 const addBookButton = document.querySelector('.add');
 const submit = document.querySelector('.submit')
 const form = document.querySelector('form');
 const cards = document.querySelector('.cards');
 
-let myLibrary = [];
-
 addBookButton.addEventListener('click', () => {
     form.classList.toggle('hidden');
-})
+});
 
 submit.addEventListener('click', (e) => {
     e.preventDefault();
@@ -21,11 +20,15 @@ submit.addEventListener('click', (e) => {
     
     let newBook = new Book(title, author, pages, read);
     addBookToLibrary(newBook);
+
+    // removes all currently displayed books and updates it
     cards.textContent = ' ';
     display();
-    
+
     form.classList.toggle('hidden');
-})
+    form.reset();
+});
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -40,9 +43,11 @@ function addBookToLibrary(Book) {
 
 let book1 = new Book("1", "2", "3", "4");
 let book2 = new Book("asdf", "adsf", "asfd", "asfd");
+let book3 = new Book('3', '3', '3', '3');
+
 addBookToLibrary(book1);
 addBookToLibrary(book2);
-
+addBookToLibrary(book3);    
 
 function display() {
     for (let i = 0; i < myLibrary.length; i++) {
@@ -51,19 +56,31 @@ function display() {
         let author = document.createElement('div');
         let pages = document.createElement('div');
         let read = document.createElement('div');
-
+        let button = document.createElement('button');
+        
         title.textContent = myLibrary[i].title;
         author.textContent = myLibrary[i].author;
         pages.textContent = myLibrary[i].pages;
         read.textContent = myLibrary[i].read;
+        button.textContent = 'X';
 
         // to be able to select it in css
         card.classList.add('card');
+        button.classList.add('remove');
+        button.dataset.number = i;
+        
+        button.addEventListener('click', (e) => {
+            myLibrary.splice(e.target.dataset.number, 1);
+            // makes the 'card container' blank and then displays it again, updating it
+            cards.textContent = ' ';
+            display();  
+        })
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
         card.appendChild(read);
+        card.appendChild(button)
 
         cards.appendChild(card);
         body.appendChild(cards);
@@ -71,4 +88,14 @@ function display() {
 }
 
 display();
+
+// problem: only one remove button works, after that, the other stop working
+
+/*document.querySelectorAll('.remove').forEach(element => element.addEventListener('click', (e) => {
+    myLibrary.splice(e.target.dataset.number, 1);
+    // makes the 'card container' blank and then displays it again, updating it
+    cards.textContent = ' ';
+    display();  
+}))*/
+
 
